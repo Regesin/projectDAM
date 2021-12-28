788f6741812d4e2e5e3abed5c2a900a0c0f6603c
MallikaNakka <mallikanakka8@gmail.com>
2021-12-28 10:36:59 +0530

    all Comments done

---
+++ damapp-order-service/src/main/java/com/order/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,89 @@
+package com.order.exceptions;
+
+import com.order.model.ApiErrors;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String message = ex.getMessage();
+        headers.add("desc","Method not allowed");
+        String  path= request.getContextPath();
+
+        //This is the body for the response entity
+        ApiErrors errors = new ApiErrors(LocalDateTime.now(),status,message,path);
+
+        return ResponseEntity.status(status).headers(headers).body(errors);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String message = ex.getMessage();
+        headers.add("desc","Method Type not allowed");
+        String  path= request.getContextPath();
+
+        //This is the body for the response entity
+        ApiErrors errors = new ApiErrors(LocalDateTime.now(),status,message,path);
+        return ResponseEntity.status(status).headers(headers).body(errors);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String message = ex.getMessage();
+        headers.add("desc","Path Variable is missing");
+        String  path= request.getContextPath();
+
+        //This is the body for the response entity
+        ApiErrors errors = new ApiErrors(LocalDateTime.now(),status,message,path);
+        return ResponseEntity.status(status).headers(headers).body(errors);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String message = ex.getMessage();
+        headers.add("desc","Request Parameter");
+        String  path= request.getContextPath();
+
+        //This is the body for the response entity
+        ApiErrors errors = new ApiErrors(LocalDateTime.now(),status,message,path);
+        return ResponseEntity.status(status).headers(headers).body(errors);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String message = ex.getMessage();
+        headers.add("desc","Data type mismatch");
+        String  path= request.getContextPath();
+
+        //This is the body for the response entity
+        ApiErrors errors = new ApiErrors(LocalDateTime.now(),status,message,path);
+        return ResponseEntity.status(status).headers(headers).body(errors);
+    }
+    @ExceptionHandler(value = OrderNotFoundException.class)
+    public ResponseEntity<Object> handleCompanyNotFound(OrderNotFoundException ex){
+        String message=ex.getMessage();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("desc","Order not found ");
+        String  path = "";
+        //This is the body for the response entity
+        ApiErrors errors=new ApiErrors(LocalDateTime.now(),HttpStatus.NOT_FOUND,message,path);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(headers).body(errors);
+
+
+    }
+
+}
---
+++ damapp-order-service/src/main/java/com/order/model/ApiErrors.java
@@ -0,0 +1,20 @@
+package com.order.model;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ApiErrors {
+
+    LocalDateTime timestamp;
+    HttpStatus status;
+    String message;
+    String path;
+}
